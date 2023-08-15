406e3fed9e897c07bb5f564b1c248544f8562357
Alexandre de Souza Ribeiro <35141274+alexandredsr@users.noreply.github.com>
2023-08-15 11:43:54 -0300

    Update infos.py

--- desafio-parada-42/infos.py
+++ desafio-parada-42/infos.py
@@ -1,9 +1,36 @@
-produto1_nome = input("Digite o nome do primeiro produto: ")
-produto1_preco = float(input("Digite o preço do primeiro produto: "))
-produto2_nome = input("Digite o nome do segundo produto: ")
-produto2_preco = float(input("Digite o preço do segundo produto: "))
-preco_total = produto1_preco + produto2_preco
-print("Produtos cadastrados:")
-print("Produto 1: {} - Preço: R${:.2f}".format(produto1_nome, produto1_preco))
-print("Produto 2: {} - Preço: R${:.2f}".format(produto2_nome, produto2_preco))
-print("Preço total: R${:.2f}".format(preco_total))
+# Inicializa listas vazias para armazenar informações dos produtos
+codigos = []
+nomes = []
+quantidades = []
+precos = []
+
+# Solicita ao usuário quantos produtos deseja cadastrar
+num_produtos = int(input("Quantos produtos você deseja cadastrar? "))
+
+# Loop para cadastrar as informações dos produtos
+for i in range(num_produtos):
+    print(f"\nCadastro do produto {i+1}:")
+    codigo = input("Código do produto: ")
+    nome = input("Nome do produto: ")
+    quantidade = int(input("Quantidade do produto: "))
+    preco = float(input("Preço do produto: "))
+    
+    # Armazena as informações nas listas
+    codigos.append(codigo)
+    nomes.append(nome)
+    quantidades.append(quantidade)
+    precos.append(preco)
+
+# Exibe as informações cadastradas
+print("\nInformações dos produtos cadastrados:")
+for i in range(num_produtos):
+    print(f"Produto {i+1}:")
+    print(f"Código: {codigos[i]}")
+    print(f"Nome: {nomes[i]}")
+    print(f"Quantidade: {quantidades[i]}")
+    print(f"Preço: R${precos[i]:.2f}")
+    print()
+
+# Calcula o valor total da compra
+valor_total = sum(quantidades[i] * precos[i] for i in range(num_produtos))
+print(f"Valor total da compra: R${valor_total:.2f}")
